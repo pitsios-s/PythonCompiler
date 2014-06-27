eab6b308453bd5731152638044d85f843877cca7
pitsios-s <stamatis.pit@gmail.com>
2014-06-27 16:00:28 +0300

    Added the latest version of sablecc(3.7)

--- compiler/sablecc.bat
+++ compiler/sablecc.bat
@@ -1,1 +1,10 @@
+rem * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+rem * This file is part of SableCC.                             *
+rem * See the file "LICENSE" for copyright information and the  *
+rem * terms and conditions for copying, distribution and        *
+rem * modification of SableCC.                                  *
+rem * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+rem
+rem IMPORTANT: Put the absolute path to sablecc.jar below.
+rem
 java -jar lib\sablecc.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
